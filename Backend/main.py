1f10e35c110cc238d8d7565e493508f8515d8026
Jesusmh06 <jesusmh06@gmail.com>
2026-04-01 02:12:35 -0500

    modificacion en el backend middleware

--- Backend/main.py
+++ Backend/main.py
@@ -26,14 +26,7 @@ app = FastAPI(
 # CORS: Frontend en Vercel + localhost para desarrollo
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        #"http://localhost:3000",
-        #"http://localhost:5173",
-        #"http://127.0.0.1:3000",
-        #"http://127.0.0.1:5173",
-        "https://project-langgraph-multiagent-scrape.vercel.app",
-    ],
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origins=["https://project-langgraph-multiagent-scrape.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
